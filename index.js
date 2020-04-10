3a9ceb5e453ecc7a0279b83516aa52d2a26d4c76
mira_arar <mira.ar230@gmail.com>
2020-04-10 15:23:11 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    let target= document.querySelector("#text");
+    target.textContent="This is really cool!";
+    
+  });
+   
+ 
